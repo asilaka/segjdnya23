d57e4787d0ac9d600157f57afb325d165a13828d
asilaka <abduzalilovasilbek27@gmail.com>
2024-09-23 16:57:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.menu')
+
+burger.onclick = () => {
+    menu.classList.toggle('open')
+}
+
+
+
+
+const modalBtn = document.querySelector('#modalBtn')
+const closeBtn = document.querySelector('#closeBtn')
+const modal = document.querySelector('.modal')
+
+
+modalBtn.onclick = () => {
+    modal.classList.add('open-modal')
+}
+
+
+
+closeBtn.onclick = () => {
+    modal.classList.remove('open-modal')
+}
+
+modal.onclick = (e) => {
+
+    console.log(e.target);
+
+    if(!e.target.closest('.modal__box')) {
+        modal.classList.remove('open-modal')
+    }
+}
